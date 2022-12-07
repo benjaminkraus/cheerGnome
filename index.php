8c876cc0d8be41a9a4340cc95f14c82c428e5179
Benjamin Kraus <ben@benkraus.com>
2022-12-06 21:52:14 -0500

    Further simplified the sunrise/sunset code.

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-function getSunset(DateTime $date) {
-    $tz = $date->getTimezone();
+function getSunsetAfter(DateTime $time) {
+    $tz = $time->getTimezone();
     $lat = 42.198585005444905;
     $lon = -71.43879697549325;
 
@@ -14,7 +14,19 @@ function getSunset(DateTime $date) {
     // date_sun_info will determine the date based on the timestamp adjusted to the default time zone.
     $default_tz = date_default_timezone_get();
     date_default_timezone_set($tz->getName());
-    $data = date_sun_info($date->getTimestamp(), $lat, $lon);
+
+    // Get the sunrise/sunset data.
+    $timestamp = $time->getTimestamp();
+    $data = date_sun_info($timestamp, $lat, $lon);
+
+    // Make sure that sunset occurs after the provided time.
+    while ($time->getTimestamp() > $data["sunset"]) {
+        // Add one day to the timestamp.
+        $timestamp = $timestamp + 86400;
+        $data = date_sun_info($timestamp, $lat, $lon);
+    }
+
+    // Restore the default timezone back to the default.
     date_default_timezone_set($default_tz);
 
     $sunrise = new DateTime('@' . $data["sunrise"], $tz);
@@ -23,15 +35,7 @@ function getSunset(DateTime $date) {
 }
 
 function getNextEvent($now) {
-    $tz = $now->getTimeZone();
-    $today = new DateTime("today", $tz);
-
-    $data = getSunset($today);
-    if ($data->{'sunset'} < $now) {
-        // After sunset. Get tomorrow's data.
-        $tomorrow = new DateTime("tomorrow", $tz);
-        $data = getSunset($tomorrow);
-    }
+    $data = getSunsetAfter($now);
 
     if ($data->{'sunrise'} > $now) {
         // Early morning. Next event is to shut off LED at sunrise.
