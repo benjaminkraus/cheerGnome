d2a193a7fb3250614ecada87384e9f1d9647b8e9
Benjamin Kraus <ben@benkraus.com>
2022-12-06 21:38:07 -0500

    Got rid of cache and simplified code for getting sunrise/sunset.

--- index.php
+++ index.php
@@ -4,66 +4,34 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-function querySunset(Datetime $date, float $lat, float $lon) {
+
+function getSunset(DateTime $date) {
+    $tz = $date->getTimezone();
+    $lat = 42.198585005444905;
+    $lon = -71.43879697549325;
+
+    // Make sure the default time zone is set correctly before calling date_sun_info.
+    // date_sun_info will determine the date based on the timestamp adjusted to the default time zone.
+    $default_tz = date_default_timezone_get();
+    date_default_timezone_set($tz->getName());
     $data = date_sun_info($date->getTimestamp(), $lat, $lon);
-    return (object) ([ "sunrise" => $data["sunrise"], "sunset" => $data["sunset"] ]);
-}
+    date_default_timezone_set($default_tz);
 
-function readCache($cacheFile) {
-    $json = false;
-    if (file_exists($cacheFile)) {
-        $json = file_get_contents($cacheFile);
-    }
-    if ($json == false) {
-        return false;
-    }
-
-    $data = json_decode($json, false);
-    $sunrise = new DateTime($data->{'sunrise'});
-    $sunset = new DateTime($data->{'sunset'});
+    $sunrise = new DateTime('@' . $data["sunrise"], $tz);
+    $sunset = new DateTime('@' . $data["sunset"], $tz);
     return (object) ([ "sunrise" => $sunrise, "sunset" => $sunset ]);
 }
 
-function writeCache($cacheFile, $data) {
-    $sunriseStr = $data->{'sunrise'}->format("c");
-    $sunsetStr = $data->{'sunset'}->format("c");
-    $json = json_encode(["sunrise" => $sunriseStr, "sunset" => $sunsetStr]);
-    file_put_contents($cacheFile, $json);
-}
-
 function getNextEvent($now) {
-    $cacheFile = "sunsetdata.txt";
-    $lat = 42.198585005444905;
-    $lon = -71.43879697549325;
     $tz = $now->getTimeZone();
-    $now = new DateTime("now", $tz);
     $today = new DateTime("today", $tz);
 
-    // Check first to see if we have sunrise/sunset cached.
-    $data = readCache($cacheFile);
-    if ($data == false) {
-        // No cache file, so read the data from the server.
-        echo "Server<br>";
-        $data = querySunset($today, $lat, $lon);
-    } else {
-        echo "Cache<br>";
-    }
-    if ($data == false) {
-        // Failed to read cache or server.
-        return false;
-    }
-
+    $data = getSunset($today);
     if ($data->{'sunset'} < $now) {
         // After sunset. Get tomorrow's data.
-        echo "After sunset<br>";
         $tomorrow = new DateTime("tomorrow", $tz);
-        $data = querySunset($tomorrow, $lat, $lon);
-        if ($data == false) {
-            // Failed to get new data from server.
-            return false;
-        }
+        $data = getSunset($tomorrow);
     }
-    writeCache($cacheFile, $data);
 
     if ($data->{'sunrise'} > $now) {
         // Early morning. Next event is to shut off LED at sunrise.
